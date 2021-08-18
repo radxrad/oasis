916bb0d2fd5a4121486dae31084983142897e429
valentinedwv <valentinedwv@users.noreply.github.com>
2021-08-18 23:37:43 +0000

    deploy: be3cacebc944012818f7a2c86c0d9a65eaf2791b

---
+++ precache-manifest.6ffc95a3d84eaf1365b94580153dfae2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92cc34a5cf7da38485195e95da25879c",
+    "url": "/oasis/index.html"
+  },
+  {
+    "revision": "17d53efa046f3fee95bd",
+    "url": "/oasis/static/css/2.ad76a385.chunk.css"
+  },
+  {
+    "revision": "b26d1064413e4ba000f9",
+    "url": "/oasis/static/css/main.a3b1bf32.chunk.css"
+  },
+  {
+    "revision": "17d53efa046f3fee95bd",
+    "url": "/oasis/static/js/2.93e2fb08.chunk.js"
+  },
+  {
+    "revision": "63871efad3f233be36e8fc2fbabe61f0",
+    "url": "/oasis/static/js/2.93e2fb08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b26d1064413e4ba000f9",
+    "url": "/oasis/static/js/main.9dbd9eac.chunk.js"
+  },
+  {
+    "revision": "4d3fe089dfbd82e18db5",
+    "url": "/oasis/static/js/runtime-main.83c2ba34.js"
+  }
+]);
