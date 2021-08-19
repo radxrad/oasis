c50263435569ca517fd6804975e6c1720a54dd7b
valentinedwv <valentinedwv@users.noreply.github.com>
2021-08-19 04:17:52 +0000

    deploy: 007472af06138a9d8b2abca4562949f6dc21e1f9

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e0ce93a65338b2086316fe3de955d50b.js"
+  "./precache-manifest.5bf5f30c4a9459b1ed664ab403f2fe7e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
