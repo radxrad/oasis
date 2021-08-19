c1521f2467ff65bd452a48dff25f77c6ac85ded8
valentinedwv <valentinedwv@users.noreply.github.com>
2021-08-19 01:50:41 +0000

    deploy: 0940e851e78d2f96d3257bf168fa759f397e178a

---
+++ precache-manifest.e0ce93a65338b2086316fe3de955d50b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd26009a399c1070ace9cadb06d71128",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17d53efa046f3fee95bd",
+    "url": "/static/css/2.ad76a385.chunk.css"
+  },
+  {
+    "revision": "493882ff95964d5987ff",
+    "url": "/static/css/main.a3b1bf32.chunk.css"
+  },
+  {
+    "revision": "17d53efa046f3fee95bd",
+    "url": "/static/js/2.93e2fb08.chunk.js"
+  },
+  {
+    "revision": "63871efad3f233be36e8fc2fbabe61f0",
+    "url": "/static/js/2.93e2fb08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "493882ff95964d5987ff",
+    "url": "/static/js/main.f79ef621.chunk.js"
+  },
+  {
+    "revision": "d77a4e4639e3231d984f",
+    "url": "/static/js/runtime-main.90b554d1.js"
+  }
+]);
