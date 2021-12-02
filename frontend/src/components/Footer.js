d456ee360c9a3a0fcdfe6dad4f4ac3e9f3050f49
ste519 <qil134@ucsd.edu>
2021-12-02 09:07:29 +0800

    Added about page

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import paths from "paths";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="logo">
+        <span>OASIS</span>
+        <img alt="radxrad-logo" src="radxrad-logo.svg" />
+      </div>
+      <a href={paths.about}>About</a>
+      <a href="#">Contact Us</a>
+      <a href="#">Terms of Use</a>
+      <a href="#">Privacy Policy</a>
+      <div className="seperator"></div>
+      <div>@2021 OASIS</div>
+    </div>
+  );
+}
+
+export default Footer;
