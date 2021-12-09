13249d8ae94f120f219c602e52386e2dcd6c29d9
ste519 <qil134@ucsd.edu>
2021-12-09 23:08:26 +0800

    Restyled dropzone

--- frontend/src/components/ResourcesTab.js
+++ frontend/src/components/ResourcesTab.js
@@ -5,8 +5,18 @@ import Dropzone from "react-dropzone-uploader";
 import { Table } from "react-bootstrap";
 
 function ResourcesTab(props) {
-  //Initial Get request here and then set the files state
   const [files, setFiles] = useState([]);
+  const customInput = () => (
+    <div className="input-wrapper">
+      <AiFillPicture />
+      <p className="label">Drag Resources Here</p>
+      <p className="upload">Or select from your computer...</p>
+      <div className="req">
+        <p>.jpg .png .pdf .csv .tsv .xlsx</p>
+        <p>max file size: 10MB</p>
+      </div>
+    </div>
+  );
 
   //Change url here to the api path
   const getUploadParams = ({ meta }) => {
@@ -16,18 +26,19 @@ function ResourcesTab(props) {
   const handleChangeStatus = ({ meta, file }, status) => {
     console.log(status, meta, file);
   };
+
   const handleSubmit = (newFiles, allFiles) => {
     setFiles([...files, ...newFiles.map((f) => f.meta)]);
     allFiles.forEach((f) => f.remove());
   };
 
-  console.log(files);
   return (
     <div className="resources">
       <Dropzone
         getUploadParams={getUploadParams}
         onChangeStatus={handleChangeStatus}
         onSubmit={handleSubmit}
+        inputContent={customInput}
         accept="image/*,audio/*,video/*"
       />
 
@@ -45,7 +56,7 @@ function ResourcesTab(props) {
               <tr key={index}>
                 <th>{item.name}</th>
                 <th>{item.size} kb</th>
-                <th style={{ textAlign: "center" }}>
+                <th>
                   {item.status === "removed" ? (
                     <AiOutlineCheckCircle />
                   ) : (
