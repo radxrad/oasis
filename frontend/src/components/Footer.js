c0dc0bb6aa9b6fccaa807db1bbd436c13b2f36d8
David Valentine <david.valentine@gmail.com>
2022-10-25 17:19:38 -0700

    foot css and layout

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,19 +1,25 @@
 import React from "react";
 import paths from "paths";
+import ListGroup from 'react-bootstrap/ListGroup';
 
 function Footer() {
   return (
     <div className="footer">
+
       <div className="logo">
         <span>OASIS</span>
         <img alt="radxrad-logo" src="radxrad-logo.svg" />
       </div>
-      <a href={paths.about}>About</a>
-      <a href="/">Contact Us</a>
-      <a href="/">Terms of Use</a>
-      <a href="/">Privacy Policy</a>
-      <div className="seperator"></div>
-      <div>@2021 OASIS</div>
+            <ListGroup horizontal>
+                <ListGroup.Item>
+                    <a href={paths.about}>About</a></ListGroup.Item>
+                 <ListGroup.Item><a href="/">Contact Us</a></ListGroup.Item>
+                 <ListGroup.Item><a href="/">Terms of Use</a></ListGroup.Item>
+                 <ListGroup.Item><a href="/">Privacy Policy</a></ListGroup.Item>
+            </ListGroup >
+        <div className="seperator"></div>
+                              <div>@2022 OASIS</div>
+
     </div>
   );
 }
